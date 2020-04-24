a8c2640fad29044ca3528bf1cf9221150b95877f
CLovinr <zggzcyg@qq.xom>
2020-04-24 21:40:19 +0800

    解决省略后缀脚本，require.get("xxxx/index")无法获取模块的问题

--- xsloader-src/core/module-def.js
+++ xsloader-src/core/module-def.js
@@ -21,7 +21,7 @@ class ModuleDef {
 		this.modules = {};
 		this.defaultModule = null;
 		this.targetDef = null;
-		if(isPreDependOn) {
+		if (isPreDependOn) {
 			this._preModule = {
 				preDependModule: true,
 				relyQueue: [],
@@ -30,7 +30,7 @@ class ModuleDef {
 				},
 				relyIt() {
 					let args = [];
-					for(let i = 0; i < arguments.length; i++) {
+					for (let i = 0; i < arguments.length; i++) {
 						args.push(arguments[i]);
 					}
 					this.relyQueue.push(args);
@@ -43,7 +43,7 @@ class ModuleDef {
 		let queue = this._preModule.relyQueue;
 		module.index = module.index || this._preIndex || 0;
 		this._preModule = null;
-		while(queue.length) {
+		while (queue.length) {
 			let args = queue.shift();
 			module.relyIt.apply(module, args);
 		}
@@ -73,27 +73,27 @@ function appendLoadingModuleDeps(defineObject) {
 	let src = defineObject.src;
 	let deps = defineObject.deps;
 	let moduleDef = theDefinedMap[src];
-	if(moduleDef) {
+	if (moduleDef) {
 		let mod = moduleDef.defaultModule && moduleDef.defaultModule.state == "loading" && moduleDef.defaultModule;
 		let _deps = mod && mod.deps;
 		U.each(_deps, (dep) => {
-			if(L.indexInArray(deps, dep) == -1) {
+			if (L.indexInArray(deps, dep) == -1) {
 				deps.push(dep);
 			}
 		});
-		if(mod) {
+		if (mod) {
 			defineObject.pushName(mod.selfname);
 		}
-		for(let k in moduleDef.modules) {
+		for (let k in moduleDef.modules) {
 			mod = moduleDef.modules[k].state == "loading" && moduleDef.modules[k];
 			_deps = mod && mod.deps;
 
-			if(mod) {
+			if (mod) {
 				defineObject.pushName(mod.selfname);
 			}
 
 			U.each(_deps, (dep) => {
-				if(L.indexInArray(deps, dep) == -1) {
+				if (L.indexInArray(deps, dep) == -1) {
 					deps.push(dep);
 				}
 			});
@@ -110,37 +110,41 @@ function appendLoadingModuleDeps(defineObject) {
 function getModule(nameOrUrl, selfname = null, ifoneThenGet = null) {
 	nameOrUrl = U.removeQueryHash(nameOrUrl);
 	let isSrc = _isSrc(nameOrUrl);
+	let config = L.config();
+	if (config && config.autoExt && /\/[^\/.]+$/.test(nameOrUrl)) { //自动后缀
+		nameOrUrl += config.autoExtSuffix;
+	}
 
 	let moduleDef = theDefinedMap[nameOrUrl];
-	if(moduleDef) {
+	if (moduleDef) {
 		let module;
-		if(isSrc) { //如果提供地址、则获取默认模块
-			if(selfname) {
+		if (isSrc) { //如果提供地址、则获取默认模块
+			if (selfname) {
 				module = moduleDef.modules[selfname];
 			} else {
 				module = moduleDef.defaultModule;
 			}
 		} else {
-			if(moduleDef.isPreDependOn) {
+			if (moduleDef.isPreDependOn) {
 				module = moduleDef._preModule;
 			} else {
 				module = moduleDef.modules[nameOrUrl];
 			}
 		}
 
-		if(module == ifoneThenGet) {
+		if (module == ifoneThenGet) {
 			module = null;
 		}
 
-		if(!module && ifoneThenGet) {
+		if (!module && ifoneThenGet) {
 			let count = 0;
 			let mod = moduleDef.defaultModule;
 			mod && mod != ifoneThenGet && count++;
-			for(let k in moduleDef.modules) {
+			for (let k in moduleDef.modules) {
 				mod = moduleDef.modules[k];
 				mod != ifoneThenGet && count++;
 			}
-			if(count == 1 && (mod.state == "loaded" || mod.state == "defined")) {
+			if (count == 1 && (mod.state == "loaded" || mod.state == "defined")) {
 				module = mod;
 			}
 		}
@@ -154,9 +158,9 @@ function getModule(nameOrUrl, selfname = null, ifoneThenGet = null) {
 //提前依赖不存在的模块
 function preDependOn(name, index) {
 	let isSrc = _isSrc(name);
-	if(isSrc) {
+	if (isSrc) {
 		throw new Error("expected name,but src:" + name);
-	} else if(theDefinedMap[name]) {
+	} else if (theDefinedMap[name]) {
 		throw new Error("already defined:" + name);
 	} else {
 		let def = new ModuleDef(null, true);
@@ -172,9 +176,9 @@ function preDependOn(name, index) {
  */
 function replaceModuleSrc(oldSrc, module) {
 	let moduleDef = theDefinedMap[oldSrc];
-	if(!moduleDef) {
+	if (!moduleDef) {
 		throw new Error("not found module:src=" + oldSrc);
-	} else if(theDefinedMap[module.src]) {
+	} else if (theDefinedMap[module.src]) {
 		throw new Error("already exists module:src=" + module.src);
 	} else {
 		moduleDef.src = module.src;
@@ -185,11 +189,11 @@ function replaceModuleSrc(oldSrc, module) {
 
 function clearEmptyModuleBySrc(src) {
 	let moduleDef = theDefinedMap[src];
-	if(moduleDef && !moduleDef.defaultModule) {
-		if(moduleDef.defaultModule) {
+	if (moduleDef && !moduleDef.defaultModule) {
+		if (moduleDef.defaultModule) {
 			throw new Error("not empty module:src=" + src);
 		} else {
-			for(let x in moduleDef.modules) {
+			for (let x in moduleDef.modules) {
 				throw new Error("not empty module:src=" + src);
 			}
 		}
@@ -204,25 +208,26 @@ function clearEmptyModuleBySrc(src) {
  */
 function setModule(name, module) {
 	let src = module.src;
-	if(!src) {
+	if (!src) {
 		throw new Error("expected module src!");
 	}
 	let moduleDef = theDefinedMap[src];
-	if(moduleDef) {
-		if(name) {
+	if (moduleDef) {
+		if (name) {
 			let lasfDef = theDefinedMap[name];
-			if(theDefinedMap[name] && lasfDef != moduleDef && !lasfDef.isPreDependOn) {
-				throw new Error("already define module:\n\tname=" + name + ",current.src=" + src + ",\n\tthat.src=" + theDefinedMap[name].src);
+			if (theDefinedMap[name] && lasfDef != moduleDef && !lasfDef.isPreDependOn) {
+				throw new Error("already define module:\n\tname=" + name + ",current.src=" + src + ",\n\tthat.src=" + theDefinedMap[
+					name].src);
 			} else {
 				moduleDef.modules[name] = module;
 				theDefinedMap[name] = moduleDef;
-				if(lasfDef && lasfDef.isPreDependOn) {
+				if (lasfDef && lasfDef.isPreDependOn) {
 					lasfDef.giveRelys(module);
 				}
 			}
 		} else {
 			//默认模块
-			if(moduleDef.defaultModule && moduleDef.defaultModule != module) {
+			if (moduleDef.defaultModule && moduleDef.defaultModule != module) {
 				throw new Error("already define default module:src=" + src);
 			} else {
 				moduleDef.defaultModule = module;
@@ -231,14 +236,15 @@ function setModule(name, module) {
 	} else {
 		moduleDef = new ModuleDef(src);
 		theDefinedMap[src] = moduleDef;
-		if(name) {
+		if (name) {
 			moduleDef.modules[name] = module;
 			let lasfDef = theDefinedMap[name];
-			if(lasfDef && !lasfDef.isPreDependOn) {
-				throw new Error("already define module:\n\tname=" + name + ",current.src=" + src + ",\n\tthat.src=" + theDefinedMap[name].src);
+			if (lasfDef && !lasfDef.isPreDependOn) {
+				throw new Error("already define module:\n\tname=" + name + ",current.src=" + src + ",\n\tthat.src=" + theDefinedMap[
+					name].src);
 			} else {
 				theDefinedMap[name] = moduleDef;
-				if(lasfDef && lasfDef.isPreDependOn) {
+				if (lasfDef && lasfDef.isPreDependOn) {
 					lasfDef.giveRelys(module);
 				}
 			}
